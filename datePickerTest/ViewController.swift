54ec1b16372c7c9bf3e1ba5d1010b8a12434f275
Alican Kurt <kurta15@itu.edu.tr>
2021-10-04 08:57:41 +0300

    Initial Commit

--- datePickerTest/ViewController.swift
+++ datePickerTest/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        datePicker.backgroundColor = .white
+        datePicker.minimumDate = Date()
+        datePicker.locale = NSLocale(localeIdentifier: "en_GB") as Locale
+        
+        stackView.isHidden = true
+    
     }
+    
+    //print(datePicker.date)
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+        stackView.isHidden = false
+    }
+    
+    @IBAction func doneClicked(_ sender: Any) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd , HH:mm"
+        dateFormatter.timeZone = .current
+        let currentDate = dateFormatter.string(from: datePicker.date)
+        firstButton.setTitle(currentDate, for: .normal)
+        stackView.isHidden = true
+    }
+    
+    
 }
+
+
+
 
